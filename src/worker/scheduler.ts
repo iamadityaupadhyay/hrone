e6f327d42587c08aeb743da7fa76d659c93e07e0
Aditya Upadhyay <aditya.upadhyay@supplyit.in>
2026-09-18 00:57:50 +0530

    style: add blank line in scheduler worker

--- src/worker/scheduler.ts
+++ src/worker/scheduler.ts
@@ -17,6 +17,7 @@ function getRandomInt(min: number, max: number): number {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
+
 /**
  * Generate humanized planned punch time (HH:mm) within min/max bounds
  */
